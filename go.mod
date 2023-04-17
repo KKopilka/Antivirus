bfdd73cf93f5d19ed873aaa8d44392c778c9376c
Yana <yana.stepanckova@yandex.ru>
2023-04-17 13:36:46 +0300

    промежуточные результаты

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require github.com/fatih/color v1.15.0
 require github.com/mattn/go-sqlite3 v1.14.16 // indirect
 
 require (
+	github.com/beevik/prefixtree v0.0.0-20190221160703-0e2fef796dd6 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	golang.org/x/sys v0.6.0 // indirect
--- internal/avs/avs.go
+++ internal/avs/avs.go
@@ -1,16 +1,14 @@
-package avs
+package main
 
 import (
-	"bytes"
 	"database/sql"
 	"errors"
 	"fmt"
 	"io"
-	"io/fs"
-	"log"
 	"os"
-	"path/filepath"
+	"strconv"
 
+	"github.com/beevik/prefixtree"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -22,159 +20,140 @@ type AVScanner struct {
 type Signature struct {
 	id          int64
 	Sign        []byte
-	sha         int64
-	offsetBegin int64 // смещение в байтах от начала
-	offsetEnd   int64
+	sha         string
+	offsetBegin string // смещение в байтах от начала
+	offsetEnd   string
 	dtype       string
 }
 
 var errFoundSign = errors.New("найдена сигнатура. Ты бара")
+var database *sql.DB
 
 func main() {
-	fmt.Println(ReadSignatureDatabase())
-}
-func (s *Signature) FindInFile(f *os.File) error {
-	virSigLen := len(s.Sign)
-	byteSlice := make([]byte, virSigLen)
-	n, err := f.ReadAt(byteSlice, s.offsetBegin)
-	if err != nil {
-		return err
-	}
-	// fmt.Println("file read result:", byteSlice)
-
-	if n < virSigLen {
-		// это ошибка, потому что мы прочитали меньше в файле, чем длина сигнатуры, вируса там нет
-		return errors.New("прочитанно меньше байтов, чем длина сигнатуры")
-	}
-
-	if bytes.Equal(byteSlice, s.Sign) {
-		// fmt.Println("found sig", s.Sign)
-		return errFoundSign
-	}
-
-	return nil
-}
-
-// поиск и загрузка сигнатур
-func NewAVScanner() *AVScanner {
-	a := &AVScanner{
-		signatures: make(map[string]Signature),
-		VirusStats: make(map[string][]string),
-	}
-
-	err := filepath.Walk("./database/signature.db", func(path string, info fs.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if info.IsDir() {
-			return nil
-		}
-
-		s, err := ReadSignatureDatabase()
-		if err != nil {
-			fmt.Println("signature not loaded", path, "err", err.Error())
-		} else {
-			a.signatures[info.Name()] = s
-		}
-		return nil
-	})
-	if err != nil {
-		log.Fatalf("%s", err.Error())
-	}
-
-	// fmt.Printf("Loaded signatures: %+v", a.signatures)
-
-	return a
-}
-
-func (avs *AVScanner) ScanFile(filepath string) error {
-	if _, ok := avs.VirusStats[filepath]; !ok {
-		avs.VirusStats[filepath] = []string{}
-	}
-
-	// open file
-	f, err := os.Open(filepath)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	for sigName, s := range avs.signatures {
-		// fmt.Println("Check sign", sigName)
-		if err := s.FindInFile(f); err != nil {
-
-			// fmt.Println("FindInFile err:", err)
-
-			// тут надо сделать тип ошибки отдельный для найденной сигнатуры и добавлять её в stats
-			if err == errFoundSign {
-				avs.VirusStats[filepath] = append(avs.VirusStats[filepath], sigName)
-			} else if err == io.EOF {
-				// fmt.Println("file is empty or EOF found", filepath)
-			} else {
-				return err
-			}
-		}
-	}
-
-	return nil
-}
-
-func ReadSignatureDatabase() (Signature, error) {
-	db, err := sql.Open("sqlite3", "./database/signatures.db")
+	db, err := sql.Open("sqlite3", "C:/Users/yanas/AV/database/signatures.db")
 	if err != nil {
 		panic(err)
 	}
+	database = db
 	defer db.Close()
+	ReadSignatureDatabase()
+}
 
-	result, err := db.Query("SELECT * FROM signatures")
+func (signature *Signature) FindInFile(f *os.File) error {
+	tree := prefixtree.New()
+
+	for _, prefix := range []
+
+
+
+
+	return nil
+	// strconv.Atoi(signature.offsetBegin)
+
+	// virSigLen := len(signature.Sign)
+	// byteSlice := make([]byte, virSigLen)
+
+	// n, err := f.ReadAt(byteSlice, signature.offsetBegin)
+	// if err != nil {
+	// 	return err
+	// }
+	// // fmt.Println("file read result:", byteSlice)
+
+	// if n < virSigLen {
+	// 	// это ошибка, потому что мы прочитали меньше в файле, чем длина сигнатуры, вируса там нет
+	// 	return errors.New("прочитанно меньше байтов, чем длина сигнатуры")
+	// }
+
+	// if bytes.Equal(byteSlice, s.Sign) {
+	// 	// fmt.Println("found sig", s.Sign)
+	// 	return errFoundSign
+	// }
+
+	
+}
+
+// поиск и загрузка сигнатур
+// func NewAVScanner() *AVScanner {
+// 	a := &AVScanner{
+// 		signatures: make(map[string]Signature),
+// 		VirusStats: make(map[string][]string),
+// 	}
+
+// 	err := filepath.Walk("./database/signature.db", func(path string, info fs.FileInfo, err error) error {
+// 		if err != nil {
+// 			return err
+// 		}
+
+// 		if info.IsDir() {
+// 			return nil
+// 		}
+
+// 		s, err := ReadSignatureDatabase()
+// 		if err != nil {
+// 			fmt.Println("signature not loaded", path, "err", err.Error())
+// 		} else {
+// 			a.signatures[info.Name()] = s
+// 		}
+// 		return nil
+// 	})
+// 	if err != nil {
+// 		log.Fatalf("%s", err.Error())
+// 	}
+
+// 	// fmt.Printf("Loaded signatures: %+v", a.signatures)
+
+// 	return a
+// }
+
+// func (avs *AVScanner) ScanFile(filepath string) error {
+// 	if _, ok := avs.VirusStats[filepath]; !ok {
+// 		avs.VirusStats[filepath] = []string{}
+// 	}
+
+// 	// open file
+// 	f, err := os.Open(filepath)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer f.Close()
+
+// 	for sigName, s := range avs.signatures {
+// 		// fmt.Println("Check sign", sigName)
+// 		if err := s.FindInFile(f); err != nil {
+
+// 			// fmt.Println("FindInFile err:", err)
+
+// 			// тут надо сделать тип ошибки отдельный для найденной сигнатуры и добавлять её в stats
+// 			if err == errFoundSign {
+// 				avs.VirusStats[filepath] = append(avs.VirusStats[filepath], sigName)
+// 			} else if err == io.EOF {
+// 				// fmt.Println("file is empty or EOF found", filepath)
+// 			} else {
+// 				return err
+// 			}
+// 		}
+// 	}
+
+// 	return nil
+// }
+
+func ReadSignatureDatabase() {
+	result, err := database.Query("SELECT * FROM signatures")
 	if err != nil {
 		panic(err)
 	}
 	defer result.Close()
-	// signature := []Signature{}
-	s := Signature{}
-	for result.Next() {
+	signature := []Signature{}
 
+	for result.Next() {
+		s := Signature{}
 		err := result.Scan(&s.id, &s.Sign, &s.sha, &s.offsetBegin, &s.offsetEnd, &s.dtype)
 
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-		// signature = append(signature, s)
+		signature = append(signature, s)
 	}
-
-	return s, nil
-
-	// sign, err := ioutil.ReadFile(filepath)
-
-	// if err != nil {
-	// 	return s, err
-	// }
-
-	// // s.sign = sign
-	// // fmt.Println("readed bytes:", sign)
-
-	// var b bytes.Buffer
-	// b.Write(sign)
-	// scanner := bufio.NewScanner(&b)
-
-	// scanner.Scan()
-	// offstr := scanner.Text()
-
-	// if offstr == "" {
-	// 	return s, errors.New("empty offset in signature file")
-	// }
-
-	// if v, err := strconv.Atoi(offstr); err != nil {
-	// 	return s, err
-	// } else {
-	// 	s.Offset = int64(v)
-	// }
-
-	// scanner.Scan()
-	// s.Sign = scanner.Bytes()
-
-	// return s, nil
+	fmt.Println(signature)
 }
