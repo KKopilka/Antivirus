9aab9162c72cc8cbad96ff0a568740e44a16e23f
Yana <yana.stepanckova@yandex.ru>
2023-04-20 03:04:25 +0300

    added a package where the functions related to the tree are located

---
+++ internal/search-tree/search-tree.go
@@ -0,0 +1,52 @@
+package searchtree
+
+import (
+	"fmt"
+	"kkopilka/AV/database"
+	"strings"
+
+	"github.com/beevik/prefixtree"
+)
+
+var actualTree *prefixtree.Tree
+
+type SignTree struct {
+	name        string
+	offsetBegin string
+	dtype       string
+}
+
+func (st *SignTree) Name() string {
+	return strings.Join([]string{st.name, st.offsetBegin, st.dtype}, ":")
+}
+
+func BuildSearchTree() error {
+	tree := prefixtree.New()
+	rows, err := database.GetConnection().Query("SELECT byte, offsetBegin, dtype FROM signatures")
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var signature []byte
+		var offsetBegin string
+		var dtype string
+		err := rows.Scan(&signature, &offsetBegin, &dtype)
+
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		s := &SignTree{name: string(signature), offsetBegin: offsetBegin, dtype: dtype}
+		tree.Add(string(signature), s)
+	}
+
+	actualTree = tree
+
+	return nil
+}
+
+func GetSearchTree() *prefixtree.Tree {
+	return actualTree
+}
